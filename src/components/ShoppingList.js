867dc34d96a468e63465fa2b80d57caf849d5b01
Tymur Bondar <tymurbondar@outlook.com>
2023-10-05 10:39:15 -0400

    filter works

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,31 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [nameFilter, setNameFilter] = useState("");
+
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleTextFilter(event){
+    const newValue = event.target.value;
+    setNameFilter(newValue);
+    console.log(newValue);
+  }
 
-    return item.category === selectedCategory;
-  });
+  const itemsToDisplay = items.filter((item) => {
+    const matchesCategory = selectedCategory === "All" || item.category === selectedCategory;
+    const matchesName = nameFilter === "" || item.name.toLowerCase().includes(nameFilter.toLowerCase());
+    
+    return matchesCategory && matchesName;
+});
+
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} handleTextFilter={handleTextFilter} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
